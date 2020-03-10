f8f84824c8086775056d031cd0b8069b2efedb7d
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-03-10 16:30:19 -0600

    creando app de paypal

--- app/traits/ConsumesExternalServices.php
+++ app/traits/ConsumesExternalServices.php
@@ -1,14 +1,27 @@
 <?php
 namespace App\traits;
+use GuzzleHttp\Client;
+
 trait ConsumesExternalServices
 {
-  function makeRequest( $method,
-                        $requestUrl,
-                        $queryParams = [],
-                        $formParams = [],
-                        $headers = [],
-                        $isJsonRequest = false)
+  function makeRequest( $method,$requestUrl,$queryParams = [],$formParams = [], $headers = [], $isJsonRequest = false)
   {
+    $client = new Client([
+      'base_uri' => $this->baseUri,
+    ]);
+    if(method_exists(this,'resolveAuthorization')){
+      $this->resolveAuthorization($queryParams,$formParams,$headers);
+    }
+    $response = $client->request([$method,
+                                  $requestUrl,
+                                  $isJsonRequest ? 'json' : 'form_params' => $formParams
+                                  'headers' => $headers,
+                                  'query' => $queryParams]);
+    $response = $response->getBody()->getContents();
+    if(method_exists(this,'decodeResponse')){
+      $response = $this->decodeResponse($response);
+    }
+    return $response;
   }
 }
 
