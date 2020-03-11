665c43b8d2578978de51c55071dcc8be8c7eee63
santiago martin <santiagomartinochoaestrada@gmail.com>
2020-03-11 16:38:13 -0600

    creando orden

--- app/Services/PaypalService.php
+++ app/Services/PaypalService.php
@@ -9,22 +9,35 @@ class PaypalService{
   protected $client_secret;
 
   use ConsumesExternalServices;
-  
+
   public function __construct(){
     $this->baseUrl = config('services.paypal.base_url');
     $this->client_id = config('services.paypal.client_id');
     $this->client_secret = config('services.paypal.client_secret');
   }
+
   public function resolveAuthorization(&$queryParams, &$formParams, &$headers){
     $headers['Authorization'] = $this->resolveAccessToken();
   }
+
   public function decodeResponse($response){
     return json_decode($response);
   }
-  public function resolveAccessToken(){
 
+  public function resolveAccessToken(){
     $credentials = base64_encode("{$this->client_id}:{$this->client_secret}");
     return "Basic {$credentials}";
   }
+
+  public function createOrder(){
+    $response = $this->makeRequest('POST','/v2/checkout/orders',
+                                    [],
+                                    [
+                                      "intent" => 'CAPTURE'
+                                    ],
+                                    [],
+                                    $isJsonRequest = true);
+  }
 }
- ?>
+
+?>
